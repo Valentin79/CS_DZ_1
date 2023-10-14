67da97996299d2c9398a62b8c5827a190eb8e6b9
Valentin <zvezdochet.79@mail.ru>
2023-10-14 22:17:03 +0300

    dz_1

---
+++ FamilyMember.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Sem_1
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+
+
+    public class FamilyMember
+    {
+        public FamilyMember Mother { get; set; }
+        public FamilyMember Fater { get; set; }
+        public string Name { get; set; }
+        public Gender Sex { get; set; }
+
+
+        void Info(int indent = 0)
+        {
+            Console.WriteLine($"{new String('-', indent)}Имя {this.Name}");
+        }
+        public virtual void Print(int indent = 0)
+        {
+            Info(indent);
+        }
+
+        public void ShowParents()
+        {
+            Console.WriteLine($"Mother: {Mother?.Name}");
+            Console.WriteLine($"Fater: {Fater?.Name}");
+        }
+
+        public void ShowGrandParents()
+        {
+            Console.WriteLine($"Grandparent: {Mother?.Fater?.Name}");
+            Console.WriteLine($"Grandparent: {Fater?.Fater?.Name}");
+        }
+
+        
+
+    }
+
+
+    public class AdultFamilyMember : FamilyMember
+    {
+        public FamilyMember Spouse { get; set; }
+        public FamilyMember[] Children { get; set; }
+
+        public override void Print(int indent = 0)
+        {
+            base.Print(indent);
+            if (Children != null)
+            {
+                foreach (var child in Children)
+                    child.Print(indent * 2);
+            }
+
+        }
+
+        public void ShowSpouse()
+        {
+            if (this.Sex == Gender.Female)
+            {
+                Console.WriteLine($"Супруг: {Spouse?.Name}");
+            }
+            else
+            {
+                Console.WriteLine($"Супруга: {Spouse?.Name}");
+            }
+        }
+
+    }
+    public class Tools
+    {
+        public static void ShowSiblings(FamilyMember person)
+        {
+            AdultFamilyMember[] parents = { (AdultFamilyMember) person.Mother, (AdultFamilyMember)person.Fater };
+            foreach (var parent in parents)
+            {
+                if (parent.Children != null) // Тут наверное проверка не нужна, потому что изначально мы идем от ребенка.
+                {
+                    foreach (FamilyMember child in parent.Children)
+                        if (!person.Name.Equals(child.Name))  // Тут надо бы использовать id, но для примера сойдет
+                        {
+                            Console.WriteLine(child.Name);
+
+                        }
+                        
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿namespace CS_Sem_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            // Бабушка
+            var person_2 = new AdultFamilyMember()
+                { Mother = null, Fater = null, Name = "Ольга", Sex = Gender.Female };
+            // Дедушка
+            var grandfather = new AdultFamilyMember()
+                { Mother = null, Fater = null, Name = "Андрей", Sex = Gender.Male };
+            // Отец
+            var father = new AdultFamilyMember() 
+                { Mother = null, Fater = grandfather, Name = "Гриша", Sex = Gender.Male, };
+            // Мать
+            var mother = new AdultFamilyMember() 
+                { Mother = person_2, Fater = null, Name = "Маша", Sex = Gender.Female };
+            // Сын
+            var son = new FamilyMember()
+                { Mother = mother, Fater = father, Name= "Вася", Sex= Gender.Male };
+            // Дядя
+            var person_1 = new AdultFamilyMember()
+                { Mother = null, Fater = grandfather, Name = "Евгений", Sex = Gender.Male };
+            // Дядя по линии матери
+            var person_3 = new AdultFamilyMember()
+                { Mother = person_2, Fater = null, Name = "Петр", Sex = Gender.Male };
+            // Жена Петра (person_3)
+            var person_5 = new AdultFamilyMember()
+            { Mother = null, Fater = null, Name = "Полина", Sex = Gender.Female };
+            // Ребенок person_3 и person_5 
+            var person_4 = new FamilyMember()
+                { Mother = person_5, Fater = person_3, Name = "Настя", Sex = Gender.Female };
+            // дочь person_5 и father
+            var person_6 = new FamilyMember()
+                { Mother = person_5, Fater = father, Name = "Катя", Sex = Gender.Female };
+
+
+            // раздаём детей
+            grandfather.Children = new FamilyMember[] { father, person_1 };
+            mother.Children = new FamilyMember[] { son,};
+            father.Children = new FamilyMember[] { son, person_6 };
+            person_2.Children = new FamilyMember[] { person_3 };
+            person_3.Children = new FamilyMember[] { person_4 };
+            person_5.Children = new FamilyMember[] { person_4, person_6 };
+
+
+            // распределяем супругов
+            mother.Spouse = father;
+            father.Spouse = mother;
+            person_3.Spouse = person_5;
+            person_5.Spouse = person_3;
+
+
+            //grandfather.Print(2);
+            //father.ShowSpouse();
+            //person_6.ShowParents();
+            //person_6.ShowGrandParents();
+            Tools.ShowSiblings(person_6);
+            Console.ReadLine();
+        }
+    }
+}
